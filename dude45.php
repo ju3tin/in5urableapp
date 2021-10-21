d013df93fc29505a05579cec2e500aae343ac07c
Justin <ju3tin@hotmail.co.uk>
2021-10-21 13:54:48 +0100

    Update dude45.php

--- dude45.php
+++ dude45.php
@@ -51,3 +51,30 @@ mysqli_close($con);
 ?>
 </body>
 </html>
+
+<?php
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "bcaaca6b3b184b";
+$password = "fd83cc9e";
+$dbname = "heroku_bb5f8388728c941";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT numberid, userName, first_name, last_name FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "numberid: " . $row["numberid"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
